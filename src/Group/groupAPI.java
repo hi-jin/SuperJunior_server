c8b752eaaa8973ff92a7122ee76482b2578b2679
JayJay-H <0911aza@naver.com>
2020-01-21 21:10:59 +0900

    #group삭제 관련 오류 수정

--- src/Group/groupAPI.java
+++ src/Group/groupAPI.java
@@ -3,6 +3,7 @@ package Group;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import DBController.deleteFromDB;
 import DBController.insertIntoDB;
 import DBController.searchFromDB;
 import DBController.updateDB;
@@ -11,6 +12,7 @@ public class groupAPI {
 	
 	public static String createGroup(String userID, String groupID) {
 		boolean createStatus = insertIntoDB.addTeam(groupID);
+		joinGroup(userID, groupID);
 		if(!createStatus) {
 			return "이미 존재하는 아이디입니다.";
 		} else {
@@ -49,7 +51,6 @@ public class groupAPI {
 			
 			throw new SQLException();
 		}catch (SQLException e) {
-			e.printStackTrace();
 			return "일치하는 그룹이 없습니다.";
 		}
 	}
@@ -57,20 +58,48 @@ public class groupAPI {
 	public static String quitGroup(String userID, String teamID) {
 		ResultSet client_list = searchFromDB.searchObjects("clients");
 		String[] updateTarget;
-		
+		StringBuilder teams = new StringBuilder();
+		boolean status = false;
 		try {
+			
 			while(client_list.next()) {
 				if(userID.equals(client_list.getString(1))) {
-					updateTarget = new String[] { teamID+";", "null" };
-					boolean status = updateDB.updateClient(userID, updateTarget);
-					if(status) {
-						return "성공적으로 탈퇴하셨습니다.";
-					}else {
-						return "DB 오류!";
+					String[] userTeams = client_list.getString(2).split(";");
+					for(String team : userTeams) {
+						if(!team.equals(teamID)) {
+							teams.append(team+";");
+						}
+					}
+					
+					String show = teams.toString();
+					if(show.equals("")) {
+						show = "null;";
+					}
+					
+					updateTarget = new String[] { show, "null" };
+					status = updateDB.updateClient(userID, updateTarget);
+				}
+			}
+			
+			client_list = searchFromDB.searchObjects("clients");
+			int count = 0;
+			while(client_list.next()) {
+				String[] userTeams = client_list.getString(2).split(";");
+				for(String team : userTeams) {
+					if(team.equals(teamID)) {
+						count++;
 					}
 				}
 			}
-			throw new SQLException();
+			
+			if(count == 0) {
+				System.out.println(deleteFromDB.deleteTeam(teamID));
+			}
+			if(status) {
+				return "성공적으로 탈퇴하셨습니다.";
+			}else {
+				return "DB 오류!";
+			}
 		} catch (SQLException e) {
 			return "넌 존재하지 않는 유저입니다;;;";
 		}
